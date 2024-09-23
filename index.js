0b405b813181a64a9d98b871bf0b19f06cbc703f
Gustavo Gettar <gus.gettar@gmail.com>
2024-09-23 11:33:41 -0300

    agregamos una ruta get de prueba

--- index.js
+++ index.js
@@ -9,4 +9,11 @@ app.listen(app.get('port'), ()=>{
 })
 //2- configurar middlewares
 
+//agregar como recibir conexiones remotas
+
 //3- configurar las rutas
+
+app.get('/prueba', (req, res)=>{
+console.log("desde la funcion de prueba")
+res.send('Desde el backend del proyecto crudCafe')
+})
