6eb18355457bd4b1612779645099ae2cc03393da
Gustavo Gettar <gus.gettar@gmail.com>
2024-09-25 09:48:00 -0300

    agregamos los middlewares cors, morgan y los de express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
-
+import cors from 'cors'
+import morgan from 'morgan'
 //1- configurar un puerto
 const app = express()
 
@@ -8,6 +9,10 @@ app.listen(app.get('port'), ()=>{
     console.info('estoy escuchando el puerto '+app.get('port'))
 })
 //2- configurar middlewares
+app.use(cors()) //permite conexiones remotas
+app.use(morgan('dev')) //nos da informacion extra en la terminal
+app.use(express.json()) //nos permite interpretar los datos del body en formato json
+app.use(express.urlencoded({extended:true})) //interpretar datos enviados en formularios
 
 //agregar como recibir conexiones remotas
 
